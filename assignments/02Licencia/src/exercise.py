7c17fea7c6610d29a4b4c5c8e9e61883dfd871e8
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-08-25 15:54:38 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,20 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    
+        e=int(input('Ingresa tu edad: '))
+    if e>=18:
+        io=str(input('¿Tienes identificación oficial? (s/n): '))
+        if io==('s'):
+            print('Trámite de licencia concedido')
+        elif io==('n'):
+            print('No cumples con los requisitos')
+        elif io!=('s') or ('n'):
+            print('Respuesta incorrecta')
+    elif e<0:
+        print('Respuesta incorrecta')
+    elif e<18:
+        print('No cumples requisitos')
     pass
 
 
