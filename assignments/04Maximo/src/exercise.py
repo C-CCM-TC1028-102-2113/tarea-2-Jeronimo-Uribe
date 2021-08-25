305c5adbca58c615e9414b096909809820508d51
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-08-25 16:01:19 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+    
+    x=int(input('Ingresa el primer número: '))
+    y=int(input('Ingresa el segundo número: '))
+    z=int(input('Ingresa el tercer número: '))
+
+    if x>y and x>z:
+        print(x)
+    elif y>x and y>z:
+        print(y)
+    elif z>x and z>y:
+        print(z)
     pass
 
 
